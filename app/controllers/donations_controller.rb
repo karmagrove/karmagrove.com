81e8ffcbf79c3ab79a78f2224f3e226e2756ecde
Joshua Montross <joshua.montross@gmail.com>
2014-12-27 20:03:41 -0500

    Batch charity payments now are allowed to not exist and the thing will not error out when attempting to vote on a batch [closes #36]

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -6,22 +6,23 @@ class DonationsController < ApplicationController
      Rails.logger.info("Params #{params}")
     if params[:batch_id]  
       @batch_id = params[:batch_id]
-      @batch = Batch.find(@batch_id)
-      @charity_ids = BatchCharity.where(:batch_id => @batch_id.to_i).map {|batch_charity| batch_charity.charity_id }
-      @charities = Charity.find(@charity_ids)
-      if @charities.nil?
-        @charities = Charity.find [99837, 99838, 99839]
-       # @product_charities = Charity.limit 3
-      end
-  
-      @batch_charities = BatchCharity.all
-      @batch.batch_products
-      @product_id = Product.where(:name => "Karma Coin").first.id
-      @purchase = Purchase.create(:product_id => @product_id)
-      @disable_nav = true
-      @disable_sidebar = true
-      if @batch.batch_charity_payments and @batch.batch_charity_payments.first.charity_payment_id
-          @charity_payment = CharityPayment.find(@batch.batch_charity_payments.first.charity_payment_id)
+      if @batch = Batch.find(@batch_id)
+        @charity_ids = BatchCharity.where(:batch_id => @batch_id.to_i).map {|batch_charity| batch_charity.charity_id }
+        @charities = Charity.find(@charity_ids)
+        if @charities.nil?
+          @charities = Charity.find [99837, 99838, 99839]
+         # @product_charities = Charity.limit 3
+        end
+    
+        @batch_charities = BatchCharity.all
+        @batch.batch_products
+        @product_id = Product.where(:name => "Karma Coin").first.id
+        @purchase = Purchase.create(:product_id => @product_id)
+        @disable_nav = true
+        @disable_sidebar = true
+        if @batch.batch_charity_payments and @batch.batch_charity_payments.first and @batch.batch_charity_payments.first.charity_payment_id
+            @charity_payment = CharityPayment.find(@batch.batch_charity_payments.first.charity_payment_id)
+        end
       end
     elsif params[:purchase_id]  # this is maybe a lil smelly - allow batches to override product preferences for charitys?
       @gift = Gift.find params[:purchase_id]
