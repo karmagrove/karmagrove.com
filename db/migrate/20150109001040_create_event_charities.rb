c09bfc0aa674b01d9450afe0975a178efdb6d6af
Joshua Montross <joshua.montross@gmail.com>
2015-01-08 19:27:26 -0500

    ads spec for event_charity, ads db migrates for revenue percentage to be stored on event, state to be stored on event, total donated for event to be stored for event

---
+++ db/migrate/20150109001040_create_event_charities.rb
@@ -0,0 +1,12 @@
+class CreateEventCharities < ActiveRecord::Migration
+  def change
+    create_table :event_charities do |t|
+      t.references :event
+      t.references :charity
+
+      t.timestamps
+    end
+    add_index :event_charities, :event_id
+    add_index :event_charities, :charity_id
+  end
+end
---
+++ db/migrate/20150109001502_add_revenue_percentage_donated_and_total_donated_and_state_to_event.rb
@@ -0,0 +1,11 @@
+class AddRevenuePercentageDonatedAndTotalDonatedAndStateToEvent < ActiveRecord::Migration
+  def change
+    add_column :events, :revenue_donation_percent, :integer
+  	add_column :events, :state, :string
+  	add_column :events, :total_donated, :float
+  	add_column :products, :reference_id, :integer
+  	add_column :products, :type, :string
+  end
+
+  add_index :products, :reference_id
+end
